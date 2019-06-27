ebc5b04c3fe6e1a88b6540fafe21fe74430676a0
meiheW <1925257530@qq.com>
2019-06-27 22:46:57 +0800

    modify followup

--- src/GameClientW.java
+++ src/GameClientW.java
@@ -26,8 +26,11 @@ public class GameClientW extends GameClient{
 
     @Override
     public boolean getFollowup(CardHand cards) {
+        int poker3 = Poker.POINTS_VALUES.get(cards.getPoker3().getPoint());
+        int poker2 = Poker.POINTS_VALUES.get(cards.getPoker2().getPoint());
+
         if(cards.getCardsType()==CardHand.HAND_TYPE_VALUE_HIC
-                && Poker.POINTS_VALUES.get(cards.getPoker3().getPoint())<=12){
+                && (poker3<12 || (poker3==12 && poker2<6))){
             return false;
         }
 
